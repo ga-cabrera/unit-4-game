c1d1c713ba7cdd656c0b75eaee0a5d6071a74362
Gerson Cabrera <ga.cabrera05@gmail.com>
2019-08-17 13:06:18 -0700

    deleted code in style.css (was being distracting, wasn't working), started my pseudo code in JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+// -------PSEUDO CODE------- \\
+// each gem has an array of numbers
+// each gem will be assigned a random number from their array. You will not know what the number is until you click on the gem.
+// there will be a random number in the "Get This Number" box from a number array.
+// when you click on a gem, their number will be added to your total score.
+// If your total score surpasses the computer's random number, you lose, you'll be alerted that you lost, your LOSE count will increase by 1, and then the game will restart.
+// If your total score is equal to the computer's random number, you win, you'll be alerted that you won, your WIN count will increase by 1, and then the game will restart.
+// whenever a new game starts, each gem will restart with a different random number from their array, the computer will give you another random number from the number array, and your total score will be 0. 
